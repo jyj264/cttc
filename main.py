e360e854791fc19d7c559b9ac1009dc708f3625e
YuJia Jin <jinyujia_2020@163.com>
2024-07-02 10:15:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -17,39 +17,42 @@ if k!="None":
 b=116
 
 if st.button("转换"):
-    if a>=b:
-        jl=a-b
-        flag=True
-    else:
-        jl=b-a
-        flag=False
-    jlsj=jl*4
-    jlh=int(jlsj/60)
-    jlm=int(jlsj%60)
-    if flag:
-        ansh=h+jlh
-    else:
-        ansh = h-jlh
-    if flag:
-        ansm=m+jlm
-    else:
-        ansm=m-jlm
-    if ansm>=60:
-        ansm-=60
-        ansh+=1
-    if ansm<0:
-        ansm+=60
-        ansh-=1
-    if ansh>=24:
-        ansh-=24
-    if ansh<0:
-        ansh+=24
-    if ansh<10:
-        ansh="0"+str(ansh)
-    if ansm < 10:
-        ansm = "0" + str(ansm)
-    res="当地理论时间  "+str(ansh)+":"+str(ansm)
-    st.info(res)
+    try:
+        if a>=b:
+            jl=a-b
+            flag=True
+        else:
+            jl=b-a
+            flag=False
+        jlsj=jl*4
+        jlh=int(jlsj/60)
+        jlm=int(jlsj%60)
+        if flag:
+            ansh=h+jlh
+        else:
+            ansh = h-jlh
+        if flag:
+            ansm=m+jlm
+        else:
+            ansm=m-jlm
+        if ansm>=60:
+            ansm-=60
+            ansh+=1
+        if ansm<0:
+            ansm+=60
+            ansh-=1
+        if ansh>=24:
+            ansh-=24
+        if ansh<0:
+            ansh+=24
+        if ansh<10:
+            ansh="0"+str(ansh)
+        if ansm < 10:
+            ansm = "0" + str(ansm)
+        res="当地理论时间  "+str(ansh)+":"+str(ansm)
+        st.info(res)
+    except TypeError:
+        st.error("请输入正确的数字！")
 
 
 
